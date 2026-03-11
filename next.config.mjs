3770c7bfd6edece2ce6019d1839bbdee8268d42d
barbermemecoin <126454243+fourmeme21@users.noreply.github.com>
2026-03-11 20:47:13 +0000

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+```js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
+```
